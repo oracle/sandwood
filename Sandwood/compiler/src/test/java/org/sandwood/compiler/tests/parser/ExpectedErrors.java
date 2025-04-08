a6722205e3171daf3c243279b1137f4882651e81
Daniel Goodman <daniel.goodman@oracle.com>
2025-04-08 12:25:21 +0100

    Correcting the expected error message so that it is independent of the file system it is running on.

--- Sandwood/compiler/src/test/java/org/sandwood/compiler/tests/parser/ExpectedErrors.java
+++ Sandwood/compiler/src/test/java/org/sandwood/compiler/tests/parser/ExpectedErrors.java
@@ -1,7 +1,7 @@
 /*
  * Sandwood
  *
- * Copyright (c) 2019-2024, Oracle and/or its affiliates
+ * Copyright (c) 2019-2025, Oracle and/or its affiliates
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
  */
@@ -45,8 +45,7 @@ public class ExpectedErrors {
                     + "Unable to perform array access on array \"bias\" as the index \"i\" is used to write to this array in a\n"
                     + "trace after it is used for this read. This means that the compiler cannot statically determine how\n"
                     + "many iterations of these loops are needed when constructing inverse operations for the model.\n"
-                    + "\n"
-                    + "\"        bias[i+1] = bias[i];\"";
+                    + "\n" + "\"        bias[i+1] = bias[i];\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
         {
@@ -58,19 +57,18 @@ public class ExpectedErrors {
             String annotatedMessage = "Error on line: 25 between columns 20 and 37\n"
                     + "Missing reference to index \"i\" when writing to array \"phi\". All loop indexes between the\n"
                     + "declaration of the array and the assignment to it must be used to ensure single assignment\n"
-                    + "semantics are maintained.\n"
-                    + "\n"
-                    + "\"                phi[j] = weights[j] * x[i][j];\"";
+                    + "semantics are maintained.\n" + "\n" + "\"                phi[j] = weights[j] * x[i][j];\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
         {
             String file = "org" + File.separator + "sandwood" + File.separator + "compiler" + File.separator + "tests"
                     + File.separator + "parser" + File.separator + "LinearRegression2Fail.sandwood";
             String message = "";
-            String annotatedMessage = "General Error\n"
-                    + "Model LinearRegression2Fail should be in the directory:\n"
-                    + "src/test/resources/testInputs/, not:\n"
-                    + "src/test/resources/testInputs/org/sandwood/compiler/tests/parser";
+            String annotatedMessage = "General Error\n" + "Model LinearRegression2Fail should be in the directory:\n"
+                    + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "testInputs"
+                    + File.separator + ", not:\n" + "src" + File.separator + "test" + File.separator + "resources"
+                    + File.separator + "testInputs" + File.separator + "org" + File.separator + "sandwood"
+                    + File.separator + "compiler" + File.separator + "tests" + File.separator + "parser";
             errors.put(file, new ErrorDesc(message, annotatedMessage + "\n"));
         }
         {
@@ -87,8 +85,7 @@ public class ExpectedErrors {
             String message = "Unable to write to \"flipsMeasured\" via \"source\" as it is an input and therefore a constant.";
             String annotatedMessage = "Error on line: 44 between columns 19 and 33\n"
                     + "Unable to write to \"flipsMeasured\" via \"source\" as it is an input and therefore a constant.\n"
-                    + "\n"
-                    + "\"            source[m] = target[m];\"";
+                    + "\n" + "\"            source[m] = target[m];\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
         {
@@ -98,8 +95,7 @@ public class ExpectedErrors {
                     + "same array to measuredFlips and flipsMeasured.";
             String annotatedMessage = "Error on line: 38 between columns 22 and 43\n"
                     + "References to arrays may not be held in multiple different arrays. Attempt to assign the same array\n"
-                    + "to measuredFlips and flipsMeasured.\n"
-                    + "\n"
+                    + "to measuredFlips and flipsMeasured.\n" + "\n"
                     + "\"        measuredFlips[l] = flipsMeasured[l];\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
@@ -110,9 +106,7 @@ public class ExpectedErrors {
                     + "the array's internal state.";
             String annotatedMessage = "Error on line: 21 between columns 14 and 20\n"
                     + "Public arrays cannot be placed into arrays as this would provide multiple access points to the\n"
-                    + "array's internal state.\n"
-                    + "\n"
-                    + "\"        flips[j] = f;\"";
+                    + "array's internal state.\n" + "\n" + "\"        flips[j] = f;\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
         {
@@ -120,8 +114,7 @@ public class ExpectedErrors {
                     + File.separator + "parser" + File.separator + "Flip1CoinMK11Fail.sandwood";
             String message = "There is no inverse to an \"or\" operation.";
             String annotatedMessage = "Error on line: 20 between columns 16 and 17\n"
-                    + "There is no inverse to an \"or\" operation.\n"
-                    + "\n"
+                    + "There is no inverse to an \"or\" operation.\n" + "\n"
                     + "\"        (false || flips[i]).observe(flipsMeasured[i]);\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
@@ -130,8 +123,7 @@ public class ExpectedErrors {
                     + File.separator + "parser" + File.separator + "HMMMetrics3Fail.sandwood";
             String message = "Incompatible types, type double[] cannot be assigned to an array of type int[][].";
             String annotatedMessage = "Error on line: 52 between columns 17 and 36\n"
-                    + "Incompatible types, type double[] cannot be assigned to an array of type int[][].\n"
-                    + "\n"
+                    + "Incompatible types, type double[] cannot be assigned to an array of type int[][].\n" + "\n"
                     + "\"\t\tmetric_g[sample] = metric_1d;\"";
             errors.put(file, new ErrorDesc(message, file + "\n" + annotatedMessage + "\n"));
         }
